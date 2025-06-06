72ea63228ebca86ee1d990e627f7167624bb0b1b
Francesco <you@example.com>
2025-06-07 01:46:17 +0200

    Add responsive max-width 480px

--- src/components/CardPath.jsx
+++ src/components/CardPath.jsx
@@ -1,7 +1,7 @@
 export default function CardPath({ path }) {
   return (
     <>
-      <div className="d-flex flex-column mt-5">
+      <div className="d-flex flex-column margin-card">
         <img className="img-paths mb-3" src={path.image} alt="" />
         <p className="paths-action">{path.description}</p>
       </div>
